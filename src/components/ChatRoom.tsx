7b1460e7d317bb0a371c68d30fc4af257ba625ac
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-07-11 15:21:16 +0200

    100vh for homepage

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -1,38 +1,50 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { TbMinusVertical } from "react-icons/tb";
-import {FiSearch} from 'react-icons/fi'
+import { FiSearch } from "react-icons/fi";
 
 const ChatRoom = () => {
-  return( <div>
-     <Container fluid className="p-0">
-      <div  style={{ height: '80vh' }} className="">
-      <Row className="p-1" style={{background: "#EFF0EF"}}>
-        <Col md={10}>
-          <img src="whatsapp.png" alt="userImage" height="38px" className="rounded"/>
+  return (
+    <div>
+      <Container fluid className="p-0">
+        <div style={{ height: "85vh" }} className="">
+          <Row className="p-1" style={{ background: "#EFF0EF" }}>
+            <Col md={10}>
+              <img
+                src="whatsapp.png"
+                alt="userImage"
+                height="38px"
+                className="rounded"
+              />
+            </Col>
+            <Col md={1}>
+              <TbMinusVertical className="text-muted" size={30} />
+            </Col>
+            <Col md={1}>
+              <FiSearch size={20} />
+            </Col>
+          </Row>
+        </div>
+        <Row className="p-1" style={{ background: "#EFF0EF", height: "10vh" }}>
+          <Col md={1} className="my-3 mx-0">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png"
+              alt="userImage"
+              height="38px"
+              className="rounded"
+            />
           </Col>
-          <Col md={1}>
-            <TbMinusVertical className="text-muted" size={30}/>
-          
+          <Col md={10}>
+            <input
+              type="text"
+              className="form-control my-3"
+              placeholder="Search"
+              aria-label="Search"
+            />
           </Col>
-          <Col md={1}>
-            <FiSearch size={20}/>
-          </Col>
-         
-         
-      </Row>
-      </div>
-      <Row className="p-1" style={{background: "#EFF0EF", height:"20vh"}}>
-        <Col md={1} className="my-3 mx-0">
-       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png" alt="userImage" height="38px" className="rounded"/>
-          </Col>
-        <Col md={10}>
-      <input type="text" className="form-control my-3" placeholder="Search" aria-label="Search" />
-      </Col>
-      </Row>
+        </Row>
       </Container>
-
-  </div>
-  )
+    </div>
+  );
 };
 
 export default ChatRoom;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,22 +5,21 @@ import { Container, Row, Col } from "react-bootstrap";
 const Home = () => {
   return (
     <Container fluid>
-       <div className="row ">
+      <div className="row" style={{ height: "5vh" }}>
         <div style={{ height: "5vh", background: "#075E55" }}>
           <div className="d-flex">
-           
             <small className="text-white"> WhatsApp </small>
           </div>
         </div>
-       
       </div>
-      <Row >
-        <Col md={4} style={{ borderRight: "1px solid #c4c4c4", height: "90vh"}}> 
- 
-
+      <Row>
+        <Col
+          md={4}
+          style={{ borderRight: "1px solid #c4c4c4", height: "95vh" }}
+        >
           <Sidebar />
         </Col>
-       
+
         <Col md={8}>
           <ChatRoom />
         </Col>
